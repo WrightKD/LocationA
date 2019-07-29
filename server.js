dca2c8dd380e1c2279a2f7868e47985366b64dc0
Kenneth <kennethdavewright18@gmail.com>
2019-07-29 11:57:18 +0200

    added porting

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
+
+port = process.env.PORT || 8080
+
 http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-}).listen(8080);
+}).listen(port);
